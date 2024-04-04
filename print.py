10e51414f888296f604a4f7c4527275fc01ceea5
Amit Panwar <21213888+amitpanwarIndia@users.noreply.github.com>
2024-04-04 22:49:23 +0530

    tanh,relu

---
+++ print.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+from graph import relu,leaky_relu,tanh
+
+random_values = [-3.5, -1.2, 0, 2.8, -4.1, 1.5, -0.7, 3.2, -2.4, 4.6]
+
+random_values_np = np.array(random_values)
+
+relu_values = relu(random_values_np)
+leaky_relu_values = leaky_relu(random_values_np)
+tanh_values = tanh(random_values_np)
+
+print("ReLU Values:")
+for val, relu_val in zip(random_values, relu_values):
+        print(f"ReLU({val}) = {relu_val}")
+
+        print("\nLeaky ReLU Values:")
+        for val, leaky_relu_val in zip(random_values, leaky_relu_values):
+                print(f"Leaky ReLU({val}) = {leaky_relu_val}")
+
+                print("\nTanh Values:")
+                for val, tanh_val in zip(random_values, tanh_values):
+                        print(f"Tanh({val}) = {tanh_val}")

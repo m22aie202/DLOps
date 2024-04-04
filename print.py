0852c4019bcccba1986e2405b2f678989a86aa09
Amit Panwar <21213888+amitpanwarIndia@users.noreply.github.com>
2024-04-04 22:45:10 +0530

    sigmoid

---
+++ print.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+from graphs import sigmoid
+
+random_values = [-3.5, -1.2, 0, 2.8, -4.1, 1.5, -0.7, 3.2, -2.4, 4.6]
+
+random_values_np = np.array(random_values)
+
+sigmoid_values = sigmoid(random_values_np)
+
+print("\nSigmoid Values:")
+for val, sig_val in zip(random_values, sigmoid_values):
+        print(f"Sigmoid({val}) = {sig_val}")
